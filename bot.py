e6601628b66a96ffd7d641f95b0e4a5200be8d2f
casperwang <casperwang2014@gmail.com>
2021-02-23 10:11:09 +0800

    in series

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import discord
-import cloze
 import json
+import ocr
 
 #Loads config.json
 with open("./config.json") as f:
@@ -21,6 +21,7 @@ async def on_message(message):
     if message.attachments and message.content == 'Ques':
         for attachment in message.attachments:
             await attachment.save(f'imgs/{attachment.filename}')
+            await message.channel.send(ocr.ocr(f'imgs/{attachment.filename}'))
     
     if message.content.startswith('Q: '):
         await message.channel.send(cloze.cloze(message.content[3:]))
--- ocr.py
+++ ocr.py
@@ -2,9 +2,10 @@ try:
     from PIL import Image
 except ImportError:
     import Image
+
 import pytesseract
 import cloze
 
-def 
-
-print(pytesseract.image_to_string(Image.open('test.png')))
+def ocr(filedir):
+    print(pytesseract.image_to_string(Image.open(filedir)))
+    return cloze.clozer(pytesseract.image_to_string(Image.open(filedir)))
