3e2de7818fc44ab0991499bb8331f5793be4d8c9
xSeanliux <zhxnliu@yahoo.com.tw>
2021-02-23 09:48:06 +0800

    Fixed bug with underscores
    checks every place for possible word placement

--- splitter.py
+++ splitter.py
@@ -1,17 +1,22 @@
 import re
 
 def splitter(string):
-    re.sub('(\s)*', ' ', string.strip()) #removes extra spaces
+    string = re.sub('[\n\r\s]+', ' ', string).strip()
     raw = re.split(" *\([A-Z]\) *", string) #Split into problem statement and options
     number = re.search('[0-9]+\s*\.', raw[0])
-    print("number = " + number.group(0))
     if(number): #Removes problem number
+        print("Question Number = " + number.group(0))
         raw[0] = raw[0].replace(number.group(0), '').strip()
     else:
         print("No problem number found.")
 
-    raw[0] = re.sub('_+', '***mask***', raw[0]) #turns underscores into ***mask***
+    raw[0] = re.sub('_+', '', raw[0]) #deletes all underscores
 
     raw = [x.lower() for x in raw] #using BERT uncased
 
     return raw
+
+
+
+
+
